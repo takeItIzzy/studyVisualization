8b3e153eeb2b41c88f4f86c2b49a52a64761e05c
赵宇轩 <yuxuan.zhao@mail.hypers.com>
2020-07-15 23:54:15 +0800

    add fonts

--- config/webpack.base.config.js
+++ config/webpack.base.config.js
@@ -3,57 +3,63 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 
 module.exports = {
-	entry: {
-		app: './src/index.tsx',
-	},
-	output: {
-		filename: '[name].[chunkhash:8].js',
-	},
-	resolve: {
-		extensions: ['.js', '.ts', '.tsx'],
-	},
-	module: {
-		rules: [
-			{
-				test: /\.tsx?$/,
-				use: ['ts-loader', 'eslint-loader'],
-				exclude: /node_modules/,
-			},
-			{
-				test: /\.css$/,
-				use: [MiniCssExtractPlugin.loader, 'css-loader'],
-			},
-			{
-				test: /\.less$/,
-				use: [
-					MiniCssExtractPlugin.loader,
-					'css-loader',
-					{
-						loader: 'less-loader',
-						options: {
-							lessOptions: {
-								javascriptEnabled: true,
-							},
-						},
-					},
-				],
-			},
-		],
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: './public/index.html',
-			inject: true,
-			favicon: path.resolve(__dirname, '../public/favicon.ico'),
-		}),
-		new MiniCssExtractPlugin({
-			filename: '[name].css',
-			chunkFilename: '[id].css',
-		}),
-	],
-	optimization: {
-		splitChunks: {
-			chunks: 'all',
-		},
-	},
+  entry: {
+    app: './src/index.tsx'
+  },
+  output: {
+    filename: '[name].[chunkhash:8].js'
+  },
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: ['ts-loader', 'eslint-loader'],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+      },
+      {
+        test: /\.less$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'less-loader',
+            options: {
+              lessOptions: {
+                javascriptEnabled: true
+              }
+            }
+          }
+        ]
+      },
+      {
+        test: /\.ttf$/,
+        loader: 'url-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'visualization',
+      template: './public/index.html',
+      inject: true,
+      favicon: path.resolve(__dirname, '../public/favicon.ico')
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css'
+    })
+  ],
+  optimization: {
+    splitChunks: {
+      chunks: 'all'
+    }
+  }
 };
