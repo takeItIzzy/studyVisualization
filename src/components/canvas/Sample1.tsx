52f66d439b8c1eff5cdc00a304ab1a7da475eddf
takeItIzzy <q1367257497@gmail.com>
2020-08-03 23:45:29 +0800

    learn canvas

--- src/components/canvas/Sample1.tsx
+++ src/components/canvas/Sample1.tsx
@@ -11,16 +11,26 @@ const Sample1 = () => {
       ctx.save(); // save canvas' state, include fillStyle, translate...
       ctx.translate(-0.5 * rectSize[0], -0.5 * rectSize[1]);
       ctx.fillStyle = 'red';
-      ctx.beginPath();
-      ctx.rect(0.5 * canvasBox.width, 0.5 * canvasBox.height, ...rectSize);
-      ctx.fill();
+      ctx.fillRect(0.5 * canvasBox.width, 0.5 * canvasBox.height, ...rectSize);
+      ctx.clearRect(
+        0.5 * canvasBox.width + 20,
+        0.5 * canvasBox.height + 20,
+        60,
+        60
+      );
+      ctx.strokeRect(
+        0.5 * canvasBox.width + 40,
+        0.5 * canvasBox.height + 40,
+        20,
+        20
+      );
       ctx.restore(); // restore the canvas' state just saved
     }
   }, [canvasRef.current]);
 
   return (
     <div className="canvas">
-      <canvas width="512" height="512" ref={canvasRef}></canvas>
+      <canvas width="512" height="512" ref={canvasRef} />
     </div>
   );
 };
--- src/components/canvas/Sample2.tsx
+++ src/components/canvas/Sample2.tsx
@@ -1,16 +1,29 @@
 import React, { useRef, useEffect, useState } from 'react';
 
-const dataSource = 'https://s5.ssl.qhres.com/static/b0695e2dd30daa64.json';
-
 const Sample2 = () => {
   const [data, setData] = useState(null);
+
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
   useEffect(() => {
-    (async () => {
-      const fetchedData = await (await fetch(dataSource)).json();
-      setData(fetchedData);
-    })();
-  }, []);
-  const canvasRef = useRef(null);
+    const canvasBox = canvasRef.current;
+    const ctx = canvasBox?.getContext('2d');
+    if (canvasBox && ctx) {
+      ctx.beginPath();
+      ctx.arc(75, 75, 50, 0, Math.PI * 2, true);
+      ctx.moveTo(110, 75);
+      ctx.arc(75, 75, 35, 0, Math.PI, false);
+      ctx.moveTo(65, 65);
+      ctx.arc(60, 65, 5, 0, Math.PI * 2, true);
+      ctx.moveTo(95, 65);
+      ctx.arc(90, 65, 5, 0, Math.PI * 2, true);
+      ctx.moveTo(115, 43);
+      ctx.arc(140, 80, 45, Math.PI * 1.3, Math.PI * 0.22, false);
+      ctx.moveTo(120, 56);
+      ctx.arc(120, 110, 55, Math.PI * 1.5, Math.PI * 0, false);
+      ctx.stroke();
+    }
+  }, [canvasRef.current]);
 
   return (
     <div className="canvas">
