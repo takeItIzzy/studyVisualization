8783f5315b8b9014a403829f583af5fb62dba03c
takeItIzzy <q1367257497@gmail.com>
2020-08-05 23:32:01 +0800

    learn canvas

--- src/components/canvas/Sample2.tsx
+++ src/components/canvas/Sample2.tsx
@@ -4,6 +4,31 @@ const Sample2 = () => {
   const [data, setData] = useState(null);
 
   const canvasRef = useRef<HTMLCanvasElement>(null);
+  const canvasRef2 = useRef<HTMLCanvasElement>(null);
+  const canvasRef3 = useRef<HTMLCanvasElement>(null);
+  const canvasRef4 = useRef<HTMLCanvasElement>(null);
+  const canvasRef5 = useRef<HTMLCanvasElement>(null);
+
+  const roundedRect = (
+    ctx: CanvasRenderingContext2D,
+    x: number,
+    y: number,
+    width: number,
+    height: number,
+    radius: number
+  ) => {
+    ctx.beginPath();
+    ctx.moveTo(x, y + radius);
+    ctx.lineTo(x, y + height - radius);
+    ctx.quadraticCurveTo(x, y + height, x + radius, y + height);
+    ctx.lineTo(x + width - radius, y + height);
+    ctx.quadraticCurveTo(x + width, y + height, x + width, y + height - radius);
+    ctx.lineTo(x + width, y + radius);
+    ctx.quadraticCurveTo(x + width, y, x + width - radius, y);
+    ctx.lineTo(x + radius, y);
+    ctx.quadraticCurveTo(x, y, x, y + radius);
+    ctx.stroke();
+  };
 
   useEffect(() => {
     const canvasBox = canvasRef.current;
@@ -25,9 +50,133 @@ const Sample2 = () => {
     }
   }, [canvasRef.current]);
 
+  useEffect(() => {
+    const canvasBox = canvasRef2.current;
+    const ctx = canvasBox?.getContext('2d');
+    if (canvasBox && ctx) {
+      for (let i = 0; i < 4; i++) {
+        for (let j = 0; j < 4; j++) {
+          ctx.beginPath();
+          const x = 25 + j * 50;
+          const y = 25 + i * 50;
+          const r = 20;
+          const startAngle = 0;
+          const endAngle = (Math.PI * (j + 1)) / 2;
+          const anticlockwise = i % 2 === 0 ? true : false;
+          ctx.arc(x, y, r, startAngle, endAngle, anticlockwise);
+          i > 1 ? ctx.fill() : ctx.stroke();
+        }
+      }
+    }
+  }, [canvasRef2.current]);
+
+  useEffect(() => {
+    const canvasBox = canvasRef3.current;
+    const ctx = canvasBox?.getContext('2d');
+    if (canvasBox && ctx) {
+      ctx.beginPath();
+      ctx.moveTo(75, 25);
+      ctx.quadraticCurveTo(25, 25, 25, 62.5);
+      ctx.quadraticCurveTo(25, 100, 50, 100);
+      ctx.quadraticCurveTo(50, 120, 30, 125);
+      ctx.quadraticCurveTo(60, 120, 65, 100);
+      ctx.quadraticCurveTo(125, 100, 125, 62.5);
+      ctx.quadraticCurveTo(125, 25, 75, 25);
+      ctx.stroke();
+    }
+  }, [canvasRef3.current]);
+
+  useEffect(() => {
+    const canvasBox = canvasRef4.current;
+    const ctx = canvasBox?.getContext('2d');
+    if (canvasBox && ctx) {
+      ctx.beginPath();
+      ctx.moveTo(75, 40);
+      ctx.bezierCurveTo(75, 37, 70, 25, 50, 25);
+      ctx.bezierCurveTo(20, 25, 20, 62.5, 20, 62.5);
+      ctx.bezierCurveTo(20, 80, 40, 102, 75, 120);
+      ctx.bezierCurveTo(110, 102, 130, 80, 130, 62.5);
+      ctx.bezierCurveTo(130, 62.5, 130, 25, 100, 25);
+      ctx.bezierCurveTo(85, 25, 75, 37, 75, 40);
+      ctx.fill();
+    }
+  }, [canvasRef4.current]);
+
+  useEffect(() => {
+    const canvasBox = canvasRef5.current;
+    const ctx = canvasBox?.getContext('2d');
+
+    if (canvasBox && ctx) {
+      roundedRect(ctx, 12, 12, 150, 150, 15);
+      roundedRect(ctx, 19, 19, 150, 150, 9);
+      roundedRect(ctx, 53, 53, 49, 33, 10);
+      roundedRect(ctx, 53, 119, 49, 16, 6);
+      roundedRect(ctx, 135, 53, 49, 33, 10);
+      roundedRect(ctx, 135, 119, 25, 49, 10);
+
+      ctx.beginPath();
+      ctx.arc(37, 37, 13, Math.PI / 7, -Math.PI / 7, false);
+      ctx.lineTo(31, 37);
+      ctx.fill();
+
+      for (let i = 0; i < 8; i++) {
+        ctx.fillRect(51 + i * 16, 35, 4, 4);
+      }
+
+      for (let i = 0; i < 6; i++) {
+        ctx.fillRect(115, 51 + i * 16, 4, 4);
+      }
+
+      for (let i = 0; i < 8; i++) {
+        ctx.fillRect(51 + i * 16, 99, 4, 4);
+      }
+
+      ctx.beginPath();
+      ctx.moveTo(83, 116);
+      ctx.lineTo(83, 102);
+      ctx.bezierCurveTo(83, 94, 89, 88, 97, 88);
+      ctx.bezierCurveTo(105, 88, 111, 94, 111, 102);
+      ctx.lineTo(111, 116);
+      ctx.lineTo(106.333, 111.333);
+      ctx.lineTo(101.666, 116);
+      ctx.lineTo(97, 111.333);
+      ctx.lineTo(92.333, 116);
+      ctx.lineTo(87.666, 111.333);
+      ctx.lineTo(83, 116);
+      ctx.fill();
+
+      ctx.fillStyle = 'white';
+      ctx.beginPath();
+      ctx.moveTo(91, 96);
+      ctx.bezierCurveTo(88, 96, 87, 99, 87, 101);
+      ctx.bezierCurveTo(87, 103, 88, 106, 91, 106);
+      ctx.bezierCurveTo(94, 106, 95, 103, 95, 101);
+      ctx.bezierCurveTo(95, 99, 94, 96, 91, 96);
+      ctx.moveTo(103, 96);
+      ctx.bezierCurveTo(100, 96, 99, 99, 99, 101);
+      ctx.bezierCurveTo(99, 103, 100, 106, 103, 106);
+      ctx.bezierCurveTo(106, 106, 107, 103, 107, 101);
+      ctx.bezierCurveTo(107, 99, 106, 96, 103, 96);
+      ctx.fill();
+
+      ctx.fillStyle = 'black';
+      ctx.beginPath();
+      ctx.arc(101, 102, 2, 0, Math.PI * 2, true);
+      ctx.fill();
+
+      ctx.beginPath();
+      ctx.arc(89, 102, 2, 0, Math.PI * 2, true);
+      ctx.fill();
+    }
+  }, [canvasRef5.current]);
+
   return (
     <div className="canvas">
-      <canvas width="512" height="512" ref={canvasRef}></canvas>
+      <canvas width="200" height="200" ref={canvasRef}></canvas>
+      <canvas width="200" height="200" ref={canvasRef2}></canvas>
+      <canvas width="200" height="200" ref={canvasRef3}></canvas>
+      <canvas width="200" height="200" ref={canvasRef4}></canvas>
+      <canvas width="200" height="200" ref={canvasRef5}></canvas>
     </div>
   );
 };
